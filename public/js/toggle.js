3f8e24f2545481c8cf282244c5a3c50e95068a82
PSNapier <46052087+PSNapier@users.noreply.github.com>
2023-01-28 17:35:15 -0600

    feat: landing page

---
+++ public/js/toggle.js
@@ -0,0 +1,10 @@
+function toggle(id) {
+	const element = document.getElementById(id);
+
+	if (element.style.display !== 'block') {
+		element.style.display = 'block';
+	}
+	else {
+		element.style.display = 'none';
+	}
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,5 +16,30 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    daisyui: {
+      themes: [
+        {
+          mytheme: {
+            "primary": "#b91c1c",
+            "secondary": "#b45309",
+            "accent": "#fbbf24",
+            "neutral": "#F7EFE7",
+            // "neutral": "#826A5C",
+            "base-100": "#22201F",
+            // "base-100": "#F7EFE7",
+            "info": "#0891b2",
+            "success": "#16a34a",
+            "warning": "#E97F14",
+            "error": "#ef4444",
+          },
+        },
+      ],
+    },
+
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require('daisyui'),
+    ],
+    // plugins: [require('@tailwindcss/forms')],
 };
